9183dede749b5f7bc5fd2abfd0b97d87fe6f7416
HoiTemmieColeg <gavintantleff@gmail.com>
2018-08-09 11:02:18 -0400

    Added Head Control
    Curently working on bullets

---
+++ Assets/CameraControlHead.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControlHead : MonoBehaviour
+{
+    public float hSpeed = 2.0F;
+    public void Update()
+    {
+        float h = hSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, 0, h);
+    }
+}
